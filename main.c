dd4632b1e87f9ef3ee048ed0752f21a37bbb9fb3
ECELucaSoler <92515171+ECELucaSoler@users.noreply.github.com>
2022-09-23 08:38:51 +0200

    init files

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "stdio.h"
+#include "Graphe/Graphe.h"
 
 
 
 int main() {
+    Graphe* g = lire_graphe("../binaries/graphe1_TP3.txt");
 
-    printf("Hello world !");
+    //afficher le graphe
+    graphe_afficher(g);
 
     return 0;
 }
