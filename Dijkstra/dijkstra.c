46cbc9842ac08ca56957a6a2e2d5e24d6d4ae9af
AntoineGrenouillet <antoine.grenouillet@edu.ece.fr>
2022-09-23 09:33:17 +0200

    recherche minimum

--- Dijkstra/dijkstra.c
+++ Dijkstra/dijkstra.c
@@ -1,5 +1,13 @@
 #include "dijkstra.h"
 
+int rechercheMinimum(int n1 , int n2){
+    int tempMin = n1;
+    if(n2 < n1){
+        tempMin = n2;
+    }
+    return tempMin ;
+}
+
 void dijkstra(pSommet* graphe, int ordre, int sommetInital, int sommetFinal){
 
     for (int i = 0; i < ordre; ++i) {
@@ -9,6 +17,7 @@ void dijkstra(pSommet* graphe, int ordre, int sommetInital, int sommetFinal){
 
     graphe[sommetInital]->couleur = 1;
     graphe[sommetInital]->pcc = 0;
+
 
 
 }
