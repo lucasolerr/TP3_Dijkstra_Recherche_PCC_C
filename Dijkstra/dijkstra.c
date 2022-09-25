25781b53c34f8cf9d38c6be372d6d493764f1040
ECELucaSoler <92515171+ECELucaSoler@users.noreply.github.com>
2022-09-25 16:35:04 +0200

    comments

--- Dijkstra/dijkstra.c
+++ Dijkstra/dijkstra.c
@@ -3,15 +3,22 @@
 
 
 int rechercherMinimum(pSommet* graphe, int ordre){
+    //Création d'un temporaire qui servira de pivot la comparaison du minimum
     int tempPoids = 1000;
+    //Création d'un temporaire qui stockera la valeur du sommet ayant le plus petit pccTotal
     int tempSommet = -1;
+    //Boucle parcourant tous les sommets
     for (int i = 0; i < ordre; i++) {
+        //Si le sommet i a le pccTotal le plus petit et qu'il n'est pas déjà marqué
         if(graphe[i]->pccTotal < tempPoids && graphe[i]->pccTotal != -1 && graphe[i]->couleur != 1){
+            //Le pivot prend son pccTotal
             tempPoids = graphe[i]->pccTotal;
+            //tempSommet prend la valeur de i
             tempSommet = i;
         }
     }
 
+    //Retour du sommet ayant le plus petit pccTotal
     return tempSommet;
 }
 
@@ -29,14 +36,20 @@ int sommetsMarques(pSommet* graphe, int ordre){
 }
 
 void actualiserPccSuccesseurs(pSommet* graphe, int ordre, int sommetPredeccesseur){
+    //Création d'un temporaire qui a la valeur du premier successeur
     pArc tempArc = graphe[sommetPredeccesseur]->arc;
 
+    //Tant qu'il reste des successeurs
     while (tempArc != NULL){
+        //Si le successeur n'a jamais été découvert
         if(graphe[tempArc->sommet]->pccTotal == -1){
+            //On met à jour son pccTotal
             graphe[tempArc->sommet]->pccTotal = graphe[sommetPredeccesseur]->pccTotal + tempArc->valeur;
+            //On actualise son prédecesseur
             graphe[tempArc->sommet]->predecesseur = sommetPredeccesseur;
 
         } else {
+            //Si le successeur a déjà été découvert et qu'il a déjà un chemin plus court
             if(graphe[tempArc->sommet]->pccTotal < graphe[sommetPredeccesseur]->pccTotal + tempArc->valeur){
                 //Rien
             } else{
@@ -48,7 +61,7 @@ void actualiserPccSuccesseurs(pSommet* graphe, int ordre, int sommetPredeccesseu
         }
 
 
-
+        //On passe au successeur suivant
         tempArc = tempArc->arc_suivant;
     }
 
@@ -57,7 +70,6 @@ void actualiserPccSuccesseurs(pSommet* graphe, int ordre, int sommetPredeccesseu
 void dijkstra(pSommet* graphe, int ordre, int sommetInital, int sommetFinal){
     //On effectue le parcours depuis le sommet Initial
     int parcours = sommetInital;
-    int pccTotal = 0;
 
     //Boucle parcourant tous les sommets
     for (int i = 0; i < ordre; i++) {
@@ -72,23 +84,22 @@ void dijkstra(pSommet* graphe, int ordre, int sommetInital, int sommetFinal){
     //Son chemin le plus cours vers lui-même est donc 0
     graphe[parcours]->pccTotal = 0;
 
-    printf("Liste de decouverte : ");
-
     //Tant qu'il reste des sommets non marqués
     while (sommetsMarques(graphe, ordre)){
 
+        //On actualise les pccTotaux des successeurs de parcours
         actualiserPccSuccesseurs(graphe, ordre, parcours);
 
+        //Parcours prend la valeur du sommet déjà découvert ayant le pccTotal le plus petit
         parcours = rechercherMinimum(graphe, ordre);
 
-        printf("%d ", parcours);
-
+        //On marque parcours, son pcc est donc connu
         graphe[parcours]->couleur = 1;
 
     }
 
-    printf("\n");
 
+    //On affiche les informations de l'exercice
     affichagePcc(graphe, ordre, sommetInital, sommetFinal);
 }
 
@@ -112,11 +123,9 @@ void affichagePcc(pSommet* graphe, int ordre, int sommetInitial, int sommetFinal
         //on affiche ce prédécesseur
         printf(" <-- %d", tempPred);
     }
-    //on saute un ligne entre chaque branche de l'arborescence
+    //on saute une ligne entre chaque branche de l'arborescence
     printf("\n");
 
+    //on affiche la longueur du parcours (nombre de sommets devant être parcouru)
     printf("Longueur du parcours : %d\n", tempCompteur);
-
-
-
 }
